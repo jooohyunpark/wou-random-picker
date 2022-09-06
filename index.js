becf1eb0ef710254e4efcf48c69abf640c7e35a0
jooohyunpark <jhp527@nyu.edu>
2022-09-06 19:41:53 -0400

    Init

---
+++ index.js
@@ -0,0 +1,25 @@
+const { argv } = require("yargs");
+const data = require("./data.json");
+
+if (argv.n <= 0 || argv.n > data.length / 2) {
+  console.log("Please check n value and try again.");
+  process.exit(1);
+}
+
+const count = argv.n || 1;
+
+const result = [];
+
+const shuffledData = data.sort((a, b) => 0.5 - Math.random());
+
+for (let i = 0; i < count; i++) {
+  const object = {
+    presenter: shuffledData.pop(),
+    reviewer: shuffledData.pop(),
+  };
+  result.push(object);
+}
+
+console.log("\n");
+console.log("🎉", result, "🎉");
+console.log("\n");
