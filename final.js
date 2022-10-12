93542a7a1c1dff215962f4cdbb04474d2bad7b31
jooohyunpark <jhp527@nyu.edu>
2022-10-12 18:47:27 -0400

    check duplicate

--- final.js
+++ final.js
@@ -3,12 +3,21 @@ const data = require("./data.json");
 const result = [];
 
 const shuffledData1 = [...data].sort((a, b) => 0.5 - Math.random());
+
 const shuffledData2 = [...data].sort((a, b) => 0.5 - Math.random());
 
 for (let i = 0; i < data.length; i++) {
+  const presenter = shuffledData1.shift();
+  let reviewer = shuffledData2.shift();
+
+  if (presenter === reviewer) {
+    shuffledData2.push(reviewer);
+    reviewer = shuffledData2.shift();
+  }
+
   const object = {
-    presenter: shuffledData1.pop(),
-    reviewer: shuffledData2.pop(),
+    presenter,
+    reviewer,
   };
   result.push(object);
 }
